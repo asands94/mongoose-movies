56138cf9fb1d9b66bbb9b85c5ac9d5eb8bada819
Ashley Sands <ashley.sands01@gmail.com>
2024-03-08 16:58:50 -0500

    reformat header

--- routes/movies.js
+++ routes/movies.js
@@ -3,7 +3,6 @@ var router = express.Router()
 
 const moviesCtrl = require('../controllers/movies')
 
-/* GET users listing. */
 router.get('/new', moviesCtrl.new)
 router.post('/', moviesCtrl.create)
 router.get('/', moviesCtrl.index)
