914e0a041c3f1f1c99ea95786be5325d276b9b01
Ashley Sands <ashley.sands01@gmail.com>
2024-03-08 14:44:02 -0500

    add index function and view

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,23 +1,29 @@
-const Movie = require('../models/movie')
+const Movie = require("../models/movie");
 
 module.exports = {
-    new: newMovie,
-    create
-}
+  new: newMovie,
+  create,
+  index,
+};
 
 function newMovie(req, res) {
-    res.render('movies/new', {errorMsg:''})
+  res.render("movies/new", { errorMsg: "" });
 }
 
-async function create(req, res){
-    req.body.nowShowing = !!req.body.nowShowing
-    req.body.cast = req.body.cast.trim();
-    if (req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/);
-    try {
-      await Movie.create(req.body);
-      res.redirect('/movies');
-    } catch (err) {
-      console.log(err);
-      res.render('movies/new', { errorMsg: err.message });
-    }
+async function create(req, res) {
+  req.body.nowShowing = !!req.body.nowShowing;
+  req.body.cast = req.body.cast.trim();
+  if (req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/);
+  try {
+    await Movie.create(req.body);
+    res.redirect("/movies");
+  } catch (err) {
+    console.log(err);
+    res.render("movies/new", { errorMsg: err.message });
+  }
+}
+
+async function index(req, res) {
+  const movies = await Movie.find({});
+  res.render("movies/index", { movies});
 }
--- routes/movies.js
+++ routes/movies.js
@@ -6,5 +6,6 @@ const moviesCtrl = require('../controllers/movies')
 /* GET users listing. */
 router.get('/new', moviesCtrl.new)
 router.post('/', moviesCtrl.create)
+router.get('/', moviesCtrl.index)
 
 module.exports = router;
