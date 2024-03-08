4547c126e7eacf154e669d6e53fdc24137ad3a5f
Ashley Sands <ashley.sands01@gmail.com>
2024-03-08 14:57:21 -0500

    add validation to movie schema

--- models/movie.js
+++ models/movie.js
@@ -4,9 +4,15 @@ const Schema = mongoose.Schema
 
 const movieSchema = new Schema(
   {
-    title: String,
-    releaseYear: { type: Number, default: 2000 },
-    mpaaRating: String,
+    title: { type: String, required: true },
+    releaseYear: {
+      type: Number,
+      default: function () {
+        return new Date().getFullYear()
+      },
+      min: 1927,
+    },
+    mpaaRating: { type: String, enum: ['G', 'PG', 'PG-13', 'R'] },
     cast: [String],
     nowShowing: { type: Boolean, default: false },
   },
