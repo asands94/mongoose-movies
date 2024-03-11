6f02899099e1a2b00f393b582c2fa0c02da30511
Ashley Sands <ashley.sands01@gmail.com>
2024-03-11 11:46:30 -0400

    add reviews controller and routes

---
+++ controllers/reviews.js
@@ -0,0 +1,16 @@
+const Movie = require('../models/movie');
+
+module.exports = {
+  create
+};
+
+async function create(req, res) {
+    const movie = await Movie.findById(req.params.id);
+    movie.reviews.push(req.body);
+    try {
+      await movie.save();
+    } catch (err) {
+      console.log(err);
+    }
+    res.redirect(`/movies/${movie._id}`);
+  }
