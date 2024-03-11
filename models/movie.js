89fe9c4e229f7fe88ef2467e3efe99b01f8f5c2f
Ashley Sands <ashley.sands01@gmail.com>
2024-03-11 11:11:20 -0400

    add review schema

--- models/movie.js
+++ models/movie.js
@@ -2,6 +2,22 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
+const reviewSchema = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    default: 5
+  }
+}, {
+  timestamps: true
+});
+
+
 const movieSchema = new Schema(
   {
     title: { type: String, required: true },
