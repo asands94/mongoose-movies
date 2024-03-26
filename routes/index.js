e63b5180bb37154adc8431912ac1157720e82131
Ashley Sands <ashley.sands01@gmail.com>
2024-03-25 21:38:54 -0400

    add auth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express')
 var router = express.Router()
+const passport = require('passport')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.redirect('/movies')
 })
 
+router.get(
+  '/auth/google',
+  passport.authenticate(
+    // Which passport strategy is being used?
+    'google',
+    {
+      // Requesting the user's profile and email
+      scope: ['profile', 'email'],
+      // Optionally force pick account every time
+      // prompt: "select_account"
+    }
+  )
+)
+
+router.get(
+  '/oauth2callback',
+  passport.authenticate('google', {
+    successRedirect: '/movies',
+    failureRedirect: '/movies',
+  })
+)
+
+router.get('/logout', function (req, res) {
+  req.logout(function () {
+    res.redirect('/movies')
+  })
+})
 module.exports = router
